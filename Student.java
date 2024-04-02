3e158242e6d72327839f658c64104840b8c73a79
jamal <jamalkacade@gmail.com>
2024-04-02 17:09:00 +0300

    student example

---
+++ Student.java
@@ -0,0 +1,20 @@
+package Java;
+
+public class Student {
+    String name;             // Declaring a reference data field named "name" of type String
+                             // This field can hold a reference to a String object
+                             // It has a default value of null when not explicitly initialized
+  
+    int age;                 // Declaring a primitive data field named "age" of type int
+                             // This field stores integer values
+                             // It has a default value of 0 when not explicitly initialized
+  
+    boolean isScienceMajor;  // Declaring a primitive data field named "isScienceMajor" of type boolean
+                             // This field stores boolean values (true or false)
+                             // It has a default value of false when not explicitly initialized
+  
+    char gender;             // Declaring a primitive data field named "gender" of type char
+                             // This field stores a single character
+                             // It has a default value of '\u0000' (which represents the null character) when not explicitly initialized
+  }
+  
