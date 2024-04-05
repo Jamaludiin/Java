4c5e1c2916bd988dc55163e502880c538d687ad7
jamal <jamalkacade@gmail.com>
2024-04-05 15:29:20 +0300

    identifiers names

--- Variables/DeclareMultipleVariables.java
+++ Variables/DeclareMultipleVariables.java
@@ -10,9 +10,18 @@ public static void main(String[] args) {
     int z = 50;
     System.out.println(x + y + z);
 
+    System.out.println();
     //AFTER
     int x1 = 5, y1 = 6, z1 = 50;
     System.out.println(x1 + y1 + z1);
+
+
+    System.out.println();
+    /*One Value to Multiple Variables
+    You can also assign the same value to multiple variables in one line: */
+    int x2, y2, z2;
+    x2 = y2 = z2 = 50;
+    System.out.println(x2 + y2 + z2);
 
 }
 
